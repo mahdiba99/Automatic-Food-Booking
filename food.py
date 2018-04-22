cb968927aacdbf536dd85d4971cf07d3492284b6
mahdiarasteh <35062638+mahdiarasteh@users.noreply.github.com>
2018-04-22 23:16:48 +0430

    Add files via upload

---
+++ food.py
@@ -0,0 +1,7 @@
+class food:
+    def __init__(self,price,name = None,id =None):
+        self.name = name
+        self.id = id
+        self.price = price
+
+
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import selenium,captcha,base64
 from selenium import webdriver
-
-
+from selenium.webdriver.common.action_chains import ActionChains
+from food import food
+love_list = ["جوجه کباب","زرشك پلو با مرغ","سبزی پلو با ماهی","چلو کباب کوبیده"]
 driver = selenium.webdriver.Chrome()
 driver.get("http://stu.iust.ac.ir/loginpage.rose")
 username = driver.find_element_by_id("j_username")
@@ -26,7 +27,70 @@ img_captcha_base64 = driver.execute_async_script("""
 with open(r"captcha.jpg", 'wb') as f:
     f.write(base64.b64decode(img_captcha_base64))
 cap = captcha.cap("captcha.jpg")
-password.send_keys("your_username")
-username.send_keys("your_pass")
+password.send_keys("0371774616")
+username.send_keys("96521011")
 captcha_input.send_keys(cap.txt)
 driver.find_element_by_id("login_btn_submit").click()
+driver.find_element_by_xpath('//img[@alt="/res?id=reserve48x48.png&dl=false&uId=132993"]').click()
+driver.find_element_by_id("nextWeekBtn").click()
+foods = []
+# priorities = {}
+# input()
+# driver.find_element_by_id('nextWeekBtn').click()
+# lst = []
+# for i in range(21):
+#         f=driver.find_element_by_id('foodNameSpan'+str(i))
+#         if '1' in f.text:
+#             lst.sort(key = lambda x:priorities[x.text.split('|')[-1]])
+#             try:
+#                 lst[0].click()
+#             except:
+#                 pass
+#             lst=[f]
+#         else:
+#             lst.append(f)
+# lst.sort(key = lambda x:priorities[x.text.split('|')[-1]])
+# try:
+#     lst[0].click()
+# except:
+#     pass
+for i in range(15):
+    try:
+        name = driver.find_element_by_id("foodNameSpan"+str(i)).text
+        element_to_hover_over = driver.find_element_by_id("foodNameSpan"+str(i))
+        hover = ActionChains(driver).move_to_element(element_to_hover_over)
+        hover.perform()
+        price = driver.find_element_by_id("foodPriceTooltip"+str(i)).text.split()[1]
+        foods.append(food(price,name,i))
+
+    except:
+        break
+day = []
+week_foods = []
+for food in foods:
+    if "1" in food.name and int(food.id)>0:
+        week_foods.append(day)
+        day = []
+    print(food.name)
+    day.append(food)
+week_foods.append(day)
+def select(foods,love_list):
+    selected = []
+    for love in love_list:
+        for day in foods:
+            for food in day:
+                if love in food.name:
+                    selected.append(food)
+                    foods.remove(day)
+    for day in foods:
+        day = sorted(day, key=lambda h: (h.price))
+        selected.append(day[0])
+    return selected
+select_list = select(week_foods,love_list)
+for item in select_list:
+    driver.find_element_by_id("userWeekReserves.selected" + str(item.id)).click()
+driver.find_element_by_id("doReservBtn").click()
+
+# else:
+            #     day = sorted(day, key=lambda h: (h.price))
+            #     # driver.find_element_by_id("userWeekReserves.selected"+str(food.id)).click()
